7dab7ad677c1d3e5ce5b511ec6279873f3069594
dak2 <dawt2h@gmail.com>
2023-05-22 08:35:47 +0900

    return first record

--- index.ts
+++ index.ts
@@ -5,14 +5,7 @@ const prisma = new PrismaClient()
 async function main() {
   await createUser();
   await findUnique();
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  })
-  console.dir(allUsers, { depth: null })
+  await findFirst();
 }
 
 async function createUser() {
@@ -38,6 +31,28 @@ async function findUnique() {
       email: 'alice@prisma.io'
     }
   })
+  console.log(user);
+}
+
+async function findFirst() {
+  const user = await prisma.user.findFirst({
+    where: {
+      posts: {
+        some: {
+          title: {
+            contains: 'Hello'
+          }
+        }
+      }
+    },
+    orderBy: {
+      email: 'asc'
+    },
+    select: {
+      id: true,
+      email: true,
+    }
+  });
   console.log(user);
 }
 
